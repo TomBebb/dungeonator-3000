c6ed35e64a0c4a6bbcb1aec0ff10500b7e8a4a46
Tom Bebbington <tophattedcoder@gmail.com>
2016-12-27 16:39:23 +0000

    Remove JS files that shouldn't be saved on Git

--- src/path/heuristic.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    function manhattan(node, goal) {
-        return Math.abs(node.x - goal.x) + Math.abs(node.y - goal.y);
-    }
-    exports.manhattan = manhattan;
-});
