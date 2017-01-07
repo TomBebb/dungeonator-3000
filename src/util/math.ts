751efa302477150430900cf86542fb6bdd90094e
Tom Bebbington <tophattedcoder@gmail.com>
2017-01-07 18:53:59 +0000

    Add documentation to math

--- src/util/math.ts
+++ src/util/math.ts
@@ -18,26 +18,28 @@ export interface Rectangle extends Point, Size {
 export function random(min: number, max: number): number {
     return Math.floor(min + Math.random() * (max - min));
 }
+/// Return a random item in `array`
 export function randomIn<T>(array: T[]): T | undefined {
     return array[Math.floor(Math.random() * array.length)]
 }
+/// Return `num` rounded to the nearest `inc`
 export function round(num: number, inc: number): number {
     return Math.round(num / inc) * inc;
 }
+/// Return a random number between `min` and `max` with the increment `inc`
 export function randomStep(min: number, max: number, inc: number): number {
     return min + round(random(min, max) - min, inc);
 }
+/// Returns true if `a` and `b` are at the same position.
 export function pointEq(a: Point, b: Point): boolean {
     return a.x === b.x && a.y === b.y;
-}
-export function pointHash(a: Point): string {
-    return `${a.x},${a.y}`;
 }
 /// Check if the rectangles `s` and `r` intersect with `spacing` needed between them to be considered intersecting.
 export function intersects(s: Rectangle, r: Rectangle, spacing: number): boolean {
     return Math.abs((s.x + s.width / 2) - (r.x + r.width / 2)) < (s.width + r.width) / 2 + spacing
     && Math.abs((s.y + s.height / 2) - (r.y + r.height / 2)) < (s.height + r.height) / 2 + spacing;
 }
+/// Return the squared distance between `a` and `b`
 export function distSquared(a: Point, b: Point) {
     return Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2);
 }
